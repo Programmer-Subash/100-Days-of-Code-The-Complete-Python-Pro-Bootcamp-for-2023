fdfe49993c441e58f69cd88d00dfade01c25d389
Subash Phuldel <programmersubash33@gmail.com>
2024-01-06 14:56:20 +0545

    day 16 coffee machine in oop version 1.0

--- day16/Coffee Machine/main.py
+++ day16/Coffee Machine/main.py
@@ -2,3 +2,19 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+menu = Menu()
+money_machine = MoneyMachine()
+coffee_machine = CoffeeMaker()
+
+state = True
+while state:
+    choice = input(f"What would you like ({menu.get_items()[:-1]}): ").lower()
+    if choice == "report":
+        coffee_machine.report()
+        money_machine.report()
+    elif choice == "off":
+        state = False
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_machine.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_machine.make_coffee(drink)
